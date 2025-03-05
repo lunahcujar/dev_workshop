b27fcb317ff9b616be8fafbc13c8993c361103ff
lunahcujar <lherrera95@ucatolica.edu.co>
2025-03-04 20:48:33 -0500

    Perimetro pentagono regular

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -153,7 +153,7 @@ class Geometria:
         Returns:
             float: Perímetro del pentágono regular
         """
-        pass
+        return 5*lado
     
     def area_hexagono_regular(self, lado, apotema):
         """
