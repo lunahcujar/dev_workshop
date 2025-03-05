9ab85a7519c46b455dceda69515010cdd19742d3
lunahcujar <lherrera95@ucatolica.edu.co>
2025-03-04 21:50:38 -0500

    Area superficie cubo hecha

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -202,7 +202,7 @@ class Geometria:
         Returns:
             float: Área de la superficie del cubo
         """
-        pass
+        return 6*lado*lado
     
     def volumen_esfera(self, radio):
         """
