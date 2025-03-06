adf3990f6bb2eb048cf04f53e90302ed34e46901
lunahcujar <lherrera95@ucatolica.edu.co>
2025-03-05 22:36:44 -0500

    Numero faltante hecho

--- src/data/data.py
+++ src/data/data.py
@@ -114,7 +114,10 @@ class Data:
         Returns:
             int: El número que falta en la secuencia
         """
-        pass
+        n = len(lista) + 1  
+        suma_esperada = n * (n + 1) // 2
+        suma_real = sum(lista)  
+        return suma_esperada - suma_real  
     
     def es_subconjunto(self, conjunto1, conjunto2):
         """
