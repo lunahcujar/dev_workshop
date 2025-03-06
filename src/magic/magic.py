efb8c89cf57c0c1e034f55415e831d3c8e2e279b
lunahcujar <lherrera95@ucatolica.edu.co>
2025-03-06 06:50:39 -0500

    Fibonacci secuencia

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -26,7 +26,7 @@ class Magic:
         Returns:
             list: Lista con los primeros n números de Fibonacci
         """
-        pass
+        return [self.fibonacci(n) for i in range (n)]
     
     def es_primo(self, n):
         """
