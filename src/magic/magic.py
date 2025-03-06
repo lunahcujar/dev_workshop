05e1adfee9465bf6e2dce9f846f0f1a6e22cffa3
lunahcujar <lherrera95@ucatolica.edu.co>
2025-03-06 07:08:42 -0500

    Factorial

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -114,7 +114,12 @@ class Magic:
         Returns:
             int: El factorial de n
         """
-        pass
+        if n == 0 or n == 1:
+            return 1
+        resultado = 1
+        for i in range(2, n + 1):
+            resultado *= i
+        return resultado
     
     def mcd(self, a, b):
         """
