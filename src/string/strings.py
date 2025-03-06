867f86f44e444180571b78fed9c298ca642dce1c
lunahcujar <lherrera95@ucatolica.edu.co>
2025-03-05 21:36:32 -0500

    Contar vocales

--- src/string/strings.py
+++ src/string/strings.py
@@ -42,7 +42,12 @@ class Strings:
         Returns:
             int: Número de vocales en la cadena
         """
-        pass
+        vocales = "aeiouAEIOU"
+        contador = 0
+        for caracter in texto:
+            if caracter in vocales:
+                contador += 1
+        return contador
     
     def contar_consonantes(self, texto):
         """
