2dd5f59ba383e6acff32afe9af047bf02a31761f
lunahcujar <lherrera95@ucatolica.edu.co>
2025-03-05 22:32:57 -0500

    Merge ordenada hecha

--- src/data/data.py
+++ src/data/data.py
@@ -68,7 +68,18 @@ class Data:
         Returns:
             list: Lista combinada y ordenada
         """
-        pass
+        resultado = []
+        i, j = 0, 0  
+        while i < len(lista1) and j < len(lista2):
+            if lista1[i] < lista2[j]:
+                resultado.append(lista1[i])
+                i += 1
+            else:
+                resultado.append(lista2[j])
+                j += 1
+
+        resultado.extend(lista1[i:])
+        resultado.extend
     
     def rotar_lista(self, lista, k):
         """
